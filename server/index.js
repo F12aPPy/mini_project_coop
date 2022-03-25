fdf6ec13270ca172d441b6dace563ea69667bf50
Teerapat Lertvuttichaikul <teerapat_lert@Teerapats-MacBook-Pro.local>
2022-03-25 17:33:33 +0700

    update function put

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 var mysql = require("mysql");
 const cors = require("cors");
+const {body, validationResult } = require('express-validator');
 
 app.use(cors());
 app.use(express.json());
@@ -25,13 +26,18 @@ app.get("/getemployee", (req, res) => {
   });
 });
 
-app.post("/create-emp", (req, res) => {
+app.post("/create-emp", body('email').isEmail() , (req, res) => {
   const first_name = req.body.first_name;
   const last_name = req.body.last_name;
   const cardnumber_id = req.body.cardnumber_id;
   const email = req.body.email;
   const phone_number = req.body.phone_number;
   const gender = req.body.gender;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
   db.query(
     "INSERT INTO employees (first_name, last_name, cardnumber_id, email, phone_number, gender) VALUE(?,?,?,?,?,?)",
@@ -50,14 +56,56 @@ app.post("/create-emp", (req, res) => {
 app.put("/emp-update", (req, res) => {
     const employee_id = req.body.employee_id;
     const phone_number = req.body.phone_number;
-    db.query("UPDATE employees SET phone_number = ? WHERE employee_id = ?", [phone_number,employee_id], (err,result) =>{
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const email = req.body.email;
+    console.log(req.body);
+    // console.log(req.body.phone_number);
+
+    if(req.body.phone_number != null){
+      db.query("UPDATE employees SET phone_number = ? WHERE employee_id = ?", [phone_number,employee_id], (err,result) =>{
         if(err){
             console.log(err);
         } else {
             res.send();
-            console.log('update complete');
+            console.log('update phone_number complete');
         }
     });
+    }
+
+    if(req.body.first_name != null){
+      db.query("UPDATE employees SET first_name = ? WHERE employee_id = ?", [first_name,employee_id], (err,result) => {
+        if(err){
+          console.log(err);
+        } else {
+          res.send();
+          console.log('update first_name complete')
+        }
+      })
+    }
+
+    if(req.body.last_name != null){
+      db.query("UPDATE employees SET last_name = ? WHERE employee_id = ?", [last_name,employee_id], (err,result) => {
+        if(err){
+          console.log(err);
+        } else {
+          res.send();
+          console.log('update last_name complete')
+        }
+      })
+    }
+
+    if(req.body.email != null){
+      db.query("UPDATE employees SET email = ? WHERE employee_id = ?", [email,employee_id], (err,result) => {
+        if(err){
+          console.log(err);
+        } else {
+          res.send();
+          console.log('update email complete')
+        }
+      })
+    }
+
 })
 
 app.delete("/emp-delete/:employee_id", (req, res) => {
